d358acf9b1c2b265a1dab7b3720b30356115c485
fabianardila <fabianardila@wposs.com>
2020-03-04 17:32:25 -0500

    Se agrega la ventana botones en la MainActivity para ingreso de estudiantes o bibliotecario. Se crea la actividad de LoginBibliotecario. Se crea el diseño de login de Bibliotecario.

---
+++ AplicacionAndroid/app/src/main/java/com/fabianardila/proyectogradouts/LoginBibliotecarioActivity.java
@@ -0,0 +1,14 @@
+package com.fabianardila.proyectogradouts;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LoginBibliotecarioActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_bibliotecario);
+    }
+}
