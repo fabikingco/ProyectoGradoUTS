4a417bc4fea8a895fe5b76c0a07483de670f3e10
fabianardila <fabianardila@wposs.com>
2020-03-10 17:12:10 -0500

    Crear actidad para busqueda.

---
+++ AplicacionAndroid/app/src/main/java/com/fabianardila/proyectogradouts/SearchLibrosActivity.java
@@ -0,0 +1,14 @@
+package com.fabianardila.proyectogradouts;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SearchLibrosActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search_libros);
+    }
+}
