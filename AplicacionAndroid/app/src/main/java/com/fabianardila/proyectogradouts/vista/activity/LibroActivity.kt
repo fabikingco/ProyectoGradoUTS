20a0a63afc2ebef4ef8709cd7bf6cb0ec8a646fd
fgardila <40304898+fgardila@users.noreply.github.com>
2020-07-24 16:19:33 -0500

    Se cambio el nombre de la aplicacion y el icono.

---
+++ AplicacionAndroid/app/src/main/java/com/fabianardila/proyectogradouts/vista/activity/LibroActivity.kt
@@ -0,0 +1,12 @@
+package com.fabianardila.proyectogradouts.vista.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.fabianardila.proyectogradouts.R
+
+class LibroActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_libro)
+    }
+}
